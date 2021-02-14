e63a203d33dee6849612ce9339c620cf05a2aa3c
gilbertlc <gilbert.lacasandile@gmail.com>
2021-02-14 22:04:16 +0800

    day 1 build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5-PhNhjCVO1k0zEnIbHgkLIhCUrygZ54",
+  authDomain: "netflixclone-d2648.firebaseapp.com",
+  projectId: "netflixclone-d2648",
+  storageBucket: "netflixclone-d2648.appspot.com",
+  messagingSenderId: "354571455659",
+  appId: "1:354571455659:web:a33f2fbc53d0c6b5353bee"
+};
