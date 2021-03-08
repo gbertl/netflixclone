d033fa63cbc829d71028522f12472ba699068c74
gilbertlc <gilbert.lacasandile@gmail.com>
2021-03-08 19:24:13 +0800

    day 6 build

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,9 +1,12 @@
 import {useEffect, useState} from "react";
 import "./PlansScreen.css";
 import db from "../firebase";
+import {useSelector} from "react-redux";
+import {selectUser} from "../features/userSlice";
 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     db.collection("products")
@@ -30,6 +33,33 @@ function PlansScreen() {
 
   console.log(products);
 
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+
+    docRef.onSnapshot(async (snap) => {
+      const {error, sessionId} = snap.data();
+
+      if (error) {
+        // Show an error to your customer and
+        // inspect your Cloud Function logs in the Firebase console.
+        alert(`An error occured: ${error.message}`);
+      }
+
+      if (sessionId) {
+        // We have a session, let's redirect to Checkout
+        // Init Stripe
+      }
+    });
+  };
+
   return (
     <div className="plansScreen">
       {Object.entries(products).map(([productId, productData]) => {
@@ -40,7 +70,9 @@ function PlansScreen() {
               <h6>{productData.description}</h6>
             </div>
 
-            <button>Subscribe</button>
+            <button onClick={() => loadCheckout(productData.prices.priceId)}>
+              Subscribe
+            </button>
           </div>
         );
       })}
